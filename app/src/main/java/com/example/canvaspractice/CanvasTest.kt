58bd8392777ba1c07b22743aa38c11caee109c74
MunkyuShin <munkyu.s.shin@gmail.com>
2016-12-31 15:06:58 +0900

    Add Rect, Oval

--- app/src/main/java/com/example/canvaspractice/CanvasTest.kt
+++ app/src/main/java/com/example/canvaspractice/CanvasTest.kt
@@ -2,6 +2,7 @@ package com.example.canvaspractice
 
 import android.content.Context
 import android.graphics.Canvas
+import android.graphics.Color
 import android.graphics.Paint
 import android.util.AttributeSet
 import android.view.View
@@ -9,15 +10,18 @@ import android.view.View
 /**
  * Created by MunkyuShin on 12/30/16.
  */
-class RectTest : View {
+class CanvasTest : View {
     constructor(context: Context?, attrs: AttributeSet?, defStyleAttr: Int) : super(context, attrs, defStyleAttr)
     constructor(context: Context?) : super(context)
     constructor(context: Context?, attrs: AttributeSet?) : super(context, attrs)
 
     override fun onDraw(canvas: Canvas?) {
         val paint = Paint()
+        paint.color = Color.WHITE
         canvas?.let {
-           it.drawRect(5f, 5f, 10f, 10f, paint)
+           it.drawRect(5f, 5f, 50f, 50f, paint)
+            it.translate(100f, 100f)
+            it.drawOval(0f, 0f, 50f, 50f, paint)
         }
 
     }
